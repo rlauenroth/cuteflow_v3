ae17ffc2f4ffd912f9cd590c66990b0a53bda8f1
unknown <rlauenroth@.rotex.de>
2011-05-05 17:24:48 +0200

    libs included

--- src/config/ProjectConfiguration.class.php
+++ src/config/ProjectConfiguration.class.php
@@ -1,5 +1,5 @@
 <?php
-require_once dirname(__FILE__) . '/../lib/symfony/lib/autoload/sfCoreAutoload.class.php';
+require_once dirname(__FILE__) . '/../lib/vendor/symfony/autoload/sfCoreAutoload.class.php';
 sfCoreAutoload::register();
 
 class ProjectConfiguration extends sfProjectConfiguration
