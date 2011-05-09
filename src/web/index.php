b0f991d0401eeee553ba25f795f1bc8c62749ea6
Rene Lauenroth <rene.lauenroth@gmail.com>
2011-05-09 14:41:29 +0200

    debugging deactivated in production controller

--- src/web/index.php
+++ src/web/index.php
@@ -3,5 +3,5 @@
 
 require_once(dirname(__FILE__).'/../config/ProjectConfiguration.class.php');
 
-$configuration = ProjectConfiguration::getApplicationConfiguration('cuteflow', 'prod', true);
+$configuration = ProjectConfiguration::getApplicationConfiguration('cuteflow', 'prod', false);
 sfContext::createInstance($configuration)->dispatch();
